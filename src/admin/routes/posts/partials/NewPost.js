624397bedd535d4a602cacbf9c50ccd0cc7699a1
danielgoodwin <daniel.goodwin@teamfa.com>
2016-12-21 12:52:33 +0000

    Made NewPost component stateless

--- src/admin/routes/posts/partials/NewPost.js
+++ src/admin/routes/posts/partials/NewPost.js
@@ -3,39 +3,29 @@ import { connect } from 'react-redux';
 import CardWithToolbar from 'admin/components/CardWithToolbar';
 import TextField from 'material-ui/TextField';
 
-class NewPost extends Component {
-
-    constructor() {
-        super();
-        this.state = {
-            text: 'initial content',
-        }
-    }
-
-    render() {
-        return (
-            <CardWithToolbar title="Create new post" btnText="Save">
-                <div>
-                    <TextField
-                        hintText="Enter the title of your post"
-                        floatingLabelText="Title"
-                        style={{ width: '100%' }}
-                    /><br />
-                    <TextField
-                        hintText="Enter the subtitle of your post"
-                        floatingLabelText="Subtitle"
-                        style={{ width: '100%' }}
-                    /><br />
-                    <TextField
-                        hintText="Your thoughts here"
-                        floatingLabelText="Body"
-                        style={{ width: '100%' }}
-                        multiLine={true}
-                    /><br />
-                </div>
-            </CardWithToolbar>
-        );
-    }
+function NewPost() {
+    return (
+        <CardWithToolbar title="Create new post" btnText="Save">
+            <div>
+                <TextField
+                    hintText="Enter the title of your post"
+                    floatingLabelText="Title"
+                    style={{ width: '100%' }}
+                /><br />
+                <TextField
+                    hintText="Enter the subtitle of your post"
+                    floatingLabelText="Subtitle"
+                    style={{ width: '100%' }}
+                /><br />
+                <TextField
+                    hintText="Your thoughts here"
+                    floatingLabelText="Body"
+                    style={{ width: '100%' }}
+                    multiLine={true}
+                /><br />
+            </div>
+        </CardWithToolbar>
+    );
 }
 
 function select(state) {
